4558bd1afdf52698cd56821e1e30040cd66fd476
Scorpio1x1 <paulpavlovic07@gmail.com>
2025-08-12 22:11:17 +0200

    error handling skeleton

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens.Experimental;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("auth")]
+    public IActionResult getAuth()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("not-found")]
+    public IActionResult getNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("bad-request")]
+    public IActionResult getBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+
+    [HttpGet("server-error")]
+    public IActionResult getServerError()
+    {
+        throw new Exception("This was a server error");
+    }
+
+}
