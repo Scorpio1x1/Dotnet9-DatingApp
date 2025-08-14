26e0e1d5a237d583291942866c701dd243f0ab4c
Scorpio1x1 <paulpavlovic07@gmail.com>
2025-08-14 20:33:59 +0200

    Section 8 - Completed, Seeding implemented, added interfaces and repository pattern at a basic level.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4558bd1afdf52698cd56821e1e30040cd66fd476")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4ed73abb14bfbea0b4b2b57628d8968a129b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
