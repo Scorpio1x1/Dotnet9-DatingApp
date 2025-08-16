d413a45db35aca014c9d0be58e90c013c4d3d7e2
Scorpio1x1 <paulpavlovic07@gmail.com>
2025-08-16 23:53:15 +0200

    Member-detailed skeleton created

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e0e1d5a237d583291942866c701dd243f0ab4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930b62b492c1276ce8ac3cf45a6fc322223bde69")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
