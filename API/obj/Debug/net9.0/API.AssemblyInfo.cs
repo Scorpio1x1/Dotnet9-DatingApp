248ef686c7ff847e3c87f7435638b24f6b35171a
Scorpio1x1 <paulpavlovic07@gmail.com>
2025-08-18 22:01:39 +0200

    App routes

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d413a45db35aca014c9d0be58e90c013c4d3d7e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36da62e23d56d46aed3fb017d203d4c94d6f5a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
