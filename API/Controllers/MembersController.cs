4f333c162f19abe7c68c477003eeb9eb805847db
Scorpio1x1 <paulpavlovic07@gmail.com>
2025-08-20 22:12:07 +0200

    Almost complete with all adding from frontend side with extra stuff added to backend

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +29,28 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> getPhotosById(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.GetMemberId();
+
+            var member = await memberRepository.GetMemberForUpdates(memberId);
+
+            if (member == null) return BadRequest("could not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+            memberRepository.Update(member); // Don't need to
+
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failedto update user");
         }
     }
 }
