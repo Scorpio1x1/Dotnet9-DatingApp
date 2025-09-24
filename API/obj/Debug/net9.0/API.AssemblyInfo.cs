016a249a349499608e9049eeacb26f299719b8a0
Pavle Pavlovic <pavle.pavlovic@student.howest.be>
2025-09-24 15:16:39 +0200

    API - Pagination Functionality

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc3a5e6b01bc990a070a751d90114ef92810544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a7a299fdcbbb34dcb656ba77cf7cd696535e67")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
