a1eeb610552213671fabbc70d6be47de10a9587f
Pavle Pavlovic <pavle.pavlovic@student.howest.be>
2025-10-05 18:25:07 +0200

    Updated home component

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10636d281fc569f5afafe2999900c23a73e73ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db76ec8c22f7bb5a15d95b257f5e120703923f52")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
