7067934e7ffc0ddd303aa2f8dd08b6fd145c55c9
Pavle Pavlovic <pavle.pavlovic@student.howest.be>
2025-10-05 14:42:45 +0200

    End of section 17 - SignalR implementation on frontend and backend

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8311cd65203506ed4f10fa9a4a11e23fd509ce7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6631d7d8c09e331e4eb29338874912486d4d865")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
